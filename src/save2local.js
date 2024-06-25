fd4286947ba17dd47e225ad3c6c864faad5102aa
Entkenntnis <Entkenntnis@users.noreply.github.com>
2024-06-25 23:47:18 +0200

    batch insert events (otherwise out of ram)

--- src/save2local.js
+++ src/save2local.js
@@ -22,7 +22,10 @@ module.exports = (App) => {
     console.log(`  ${events.length} rows loaded`)
 
     console.log('  Saving events ...')
-    await LOCALAPP.db.Event.bulkCreate(events, { silent: true })
+    for (let i = 0; i < events.length; i += 10000)
+      await LOCALAPP.db.Event.bulkCreate(events.slice(i, i + 10000), {
+        silent: true,
+      })
 
     console.log('Loading shares ...')
     const shares = await App.db.QuestShare.findAll({ raw: true })
@@ -31,12 +34,12 @@ module.exports = (App) => {
     console.log('  Saving shares ...')
     await LOCALAPP.db.QuestShare.bulkCreate(shares, { silent: true })
 
-    console.log('Loading solutions ...')
-    const solutions = await App.db.SolutionLog.findAll({ raw: true })
-    console.log(`  ${solutions.length} rows loaded`)
+    // console.log('Loading solutions ...')
+    // const solutions = await App.db.SolutionLog.findAll({ raw: true })
+    // console.log(`  ${solutions.length} rows loaded`)
 
-    console.log('  Saving solutions ...')
-    await LOCALAPP.db.SolutionLog.bulkCreate(solutions, { silent: true })
+    // console.log('  Saving solutions ...')
+    // await LOCALAPP.db.SolutionLog.bulkCreate(solutions, { silent: true })
 
     console.log('Loading legacyShares ...')
     const legacyShares = await App.db.LegacyShare.findAll({ raw: true })
